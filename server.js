41dba47811819bb88fff076a879cc0ab6cd1eb4c
Nama Soukouna <nsoukouna3@gmail.com>
2022-10-31 00:08:05 -0400

    calculator form

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express= require("express");
+const bodyParser = require("body-parser");
 
 const app= express();
 const port=3000;
+app.use(bodyParser.urlencoded({extend:true}))
 
-app.get("/", (request, response)=>{
-    response.send("<h1>Hello World</h1>")
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/index.html");
+    
 })
 
+app.post("/",(req, res)=>{
+    let num1= Number(req.body.num1);
+    let num2= Number(req.body.num2);
+    let result = num1+ num2;
+    res.send("The result of the calculation is = "+ result)
+})
 
 app.listen(port,()=>{
     console.log("Server is running on port "+ port);
